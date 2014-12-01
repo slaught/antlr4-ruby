5fcd4a8d1404ac0fb4e96eeb3b644ab0c33b4378
Chad Slaughter <chad.slaughter@gmail.com>
2014-12-01 14:41:28 -0600

    reformat keywords

--- tool/src/org/antlr/v4/codegen/RubyTarget.java
+++ tool/src/org/antlr/v4/codegen/RubyTarget.java
@@ -46,9 +46,9 @@ public class RubyTarget extends Target {
 	protected static final String[] rubyKeywords = {
     "BEGIN", "END", "__ENCODING__", "__END__", "__FILE__", "__LINE__", "alias",
     "and", "begin", "break", "case", "class", "def", "defined?", "do", "else",
-    "elsif", "end", "ensure", "false", "for", "if", "in", "module", "next", "nil",
-    "not", "or", "redo", "rescue", "retry", "return", "self", "super", "then",
-    "true", "undef", "unless", "until", "when", "while", "yield",
+    "elsif", "end", "ensure", "false", "for", "if", "in", "module", "next",
+    "nil", "not", "or", "redo", "rescue", "retry", "return", "self", "super",
+    "then", "true", "undef", "unless", "until", "when", "while", "yield",
     "Object", "Class", "Module", "Kernel", "Rubinius"
 	};
 
